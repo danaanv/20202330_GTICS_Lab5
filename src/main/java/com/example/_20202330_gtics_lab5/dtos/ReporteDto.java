f8a372c56410fe8afe04081a5ac26f850aeea481
Dana <a20202330@pucp.edu.pe>
2023-05-01 18:58:49 -0500

    Pregunta 3

---
+++ src/main/java/com/example/_20202330_gtics_lab5/dtos/ReporteDto.java
@@ -0,0 +1,10 @@
+package com.example._20202330_gtics_lab5.dtos;
+
+public interface ReporteDto {
+
+    String getPuestoNombre();
+    Double getSalmaximo();
+    Double getSalminimo();
+    Double getSaltotal();
+    Double getSalpromedio();
+}
