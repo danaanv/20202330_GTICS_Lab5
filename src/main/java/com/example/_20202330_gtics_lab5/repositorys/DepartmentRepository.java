a8286290872e9b082f2a72c2246016932f6fc1e2
Dana <a20202330@pucp.edu.pe>
2023-05-01 17:42:15 -0500

    Cambios de la P2 1.1

--- src/main/java/com/example/_20202330_gtics_lab5/repositorys/DepartmentRepository.java
+++ src/main/java/com/example/_20202330_gtics_lab5/repositorys/DepartmentRepository.java
@@ -2,6 +2,14 @@ package com.example._20202330_gtics_lab5.repositorys;
 
 import com.example._20202330_gtics_lab5.entitys.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface DepartmentRepository extends JpaRepository<Department, Integer> {
+
+    @Query(value = "select * from departments where department_name = ?1",
+            nativeQuery = true)
+    List<Department> findByDepartmentName(String nombre);
+
 }
