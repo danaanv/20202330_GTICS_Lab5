e0d1bc5789e69f9dde2abeb99b53726204c1ca6a
danaanv <a20202330@pucp.edu.pe>
2023-04-30 20:46:01 -0500

    Inicial Commit

---
+++ src/main/java/com/example/_20202330_gtics_lab5/Application.java
@@ -0,0 +1,13 @@
+package com.example._20202330_gtics_lab5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/_20202330_gtics_lab5/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example._20202330_gtics_lab5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
