9e4d014c254950f57de57072c7466bd1f1b56702
Dana <a20202330@pucp.edu.pe>
2023-05-01 19:11:08 -0500

    Git final P1

--- src/main/java/com/example/_20202330_gtics_lab5/controllers/Controller.java
+++ src/main/java/com/example/_20202330_gtics_lab5/controllers/Controller.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @org.springframework.stereotype.Controller
 public class Controller {
 
-    @GetMapping("/")
+    @GetMapping("/principal")
     public String principal(){
-        return "navbar";
+        return "muestra_P1";
     }
     // Esto es solo para que se corriera y ya:)
 }
